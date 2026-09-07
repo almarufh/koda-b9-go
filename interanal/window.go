d9ae268d2c3c3f5556d663f3e4bf5cbd2ef34c46
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-07 19:57:28 +0700

    feat: func create window border start

---
+++ interanal/window.go
@@ -0,0 +1,16 @@
+package interanal
+
+import "fmt"
+
+func StarWindow(panjang int8, lebar int8) {
+	for i := range panjang {
+		for j := range lebar {
+			if i == 0 || i == panjang-1 || j == 0 || j == lebar-1 {
+				fmt.Print("* ")
+			} else {
+				fmt.Print("  ")
+			}
+		}
+		fmt.Println()
+	}
+}
