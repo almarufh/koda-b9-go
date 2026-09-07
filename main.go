ace856eae4281f5bdd435598510f1f35b5c3b2d2
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-07 13:27:54 +0700

    feat: create function in golang

--- main.go
+++ main.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
+func hitungLuas(panjang int8, lebar int8) int16 {
+	return int16(panjang * lebar)
+}
+
+func hitungKeliling(panjang int8, lebar int8) int16 {
+	return int16(2 * panjang * lebar)
+}
+
+func luasKeliling(panjang int8, lebar int8) (luas int16, keliling int16) {
+	return int16(panjang * lebar), int16(2 * panjang * lebar)
+}
+
 func main() {
-	fmt.Println("Hello")
-	fmt.Println("World")
+	const panjang int8 = 4
+	const lebar int8 = 8
+	luas := hitungLuas(panjang, lebar)
+	keliling := hitungKeliling(panjang, lebar)
+	l, k := luasKeliling(panjang, lebar)
+	fmt.Printf("Luas : %d\n", luas)
+	fmt.Printf("Keliling : %d\n", keliling)
+	fmt.Printf("L : %d\nK : %d ", l, k)
 }
