81d552d7101c36265909f2763816739037fb45c2
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-07 20:05:43 +0700

    feat: return slice for use

--- internal/slice.go
+++ internal/slice.go
@@ -5,15 +5,17 @@ import (
 	"slices"
 )
 
-func InsertSlice() {
+func InsertSlice() []int8 {
 	oldSlice := []int8{50, 75, 66, 20, 32, 90}
 	findIndex := slices.Index(oldSlice, 66) + 1
 	leftSlice := oldSlice[:findIndex]
 	leftSlice = slices.Grow(leftSlice, len(oldSlice))
 	leftSlice = append(leftSlice, 88)
-	oldSlice = append(leftSlice, oldSlice[findIndex:]...)
+	finalSlice := append(leftSlice, oldSlice[findIndex:]...)
 
-	for i, v := range oldSlice {
+	for i, v := range finalSlice {
 		fmt.Printf("%d. %d\n", i, v)
 	}
+
+	return finalSlice
 }
