05cbd56bf7353e1ca7c5477ed16f7c4d3749c533
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-08 21:21:20 +0700

    feat: struct and metode payment fiktif

---
+++ internal/payments/fiktif.go
@@ -0,0 +1,40 @@
+package payments
+
+import "fmt"
+
+type Fiktif struct {
+	Total []uint32
+}
+
+func (o *Fiktif) newTFiktif(total []uint32) {
+	o.Total = append(o.Total, total...)
+}
+
+func (o *Fiktif) Payment(listPrice []uint32) error {
+
+	if len(listPrice) < 1 {
+		return fmt.Errorf("List tagihan kosong")
+	}
+
+	var tagihan uint32 = 0
+	for _, v := range listPrice {
+		tagihan += v
+	}
+
+	if tagihan <= 0 {
+		return fmt.Errorf("Tagihan sama dengan atau kurang dari 0 ")
+	}
+	o.newTFiktif(listPrice)
+	return nil
+}
+
+func (o *Fiktif) CetakStrukFiktif() {
+
+	var tagihan uint32
+
+	for _, v := range o.Total {
+		tagihan += v
+	}
+
+	fmt.Println(Struk(tagihan, "FIKTIF"))
+}
