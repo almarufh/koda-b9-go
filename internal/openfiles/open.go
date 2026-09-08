ea6912d0d5b740e04fd3a587f6a38e54acc5df68
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-08 12:03:41 +0700

    feat: create func open files

---
+++ internal/openfiles/open.go
@@ -0,0 +1,33 @@
+package openfiles
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Open(url string) (result string, err error) {
+	file, err := os.Open(url)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	defer file.Close()
+	cnt, err := io.ReadAll(file)
+
+	if err != nil {
+		panic("Terjadi panic saat mebaca file !")
+	}
+
+	fmt.Println(string(cnt))
+
+	return string(cnt), nil
+}
